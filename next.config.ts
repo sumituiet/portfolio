16cbb6404a08f6911568f21c7e6e4f4e52841560
Sumit <110224690+sumituiet@users.noreply.github.com>
2024-12-09 21:28:03 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
 };
 
 export default nextConfig;
